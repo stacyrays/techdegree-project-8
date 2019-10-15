eb3227a4afb49225aca28926595f9d0f191a8f48
Stacy <stacyrays@gmail.com>
2019-10-14 19:21:43 -0700

    Fixed the top level 404 error

--- app.js
+++ app.js
@@ -34,8 +34,12 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  if (err.status === 404) {
+    res.render("page-not-found");
+  } else {
+    res.status(err.status || 500);
+    res.render("error");
+  }
 });
 
 module.exports = app;
